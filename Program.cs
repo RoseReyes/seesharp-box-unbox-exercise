5e7df5ceeb9914da13df04e5eb65a3f2aa8c6f4e
Rose Reyes <roseannlorinreyes@gmail.com>
2018-04-04 14:55:06 -0700

    added objects boxing/unboxing exercise

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace box_unbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> multiple_type = new List<object>();
+            
+            multiple_type.Add((int)7);
+            multiple_type.Add((int)28);
+            multiple_type.Add((int)-1);
+            multiple_type.Add((bool)true);
+            multiple_type.Add((string)"chair");
+            
+            foreach (object item in multiple_type)
+            {
+                 if (item is int){
+                     //THIS ONLY ADDS UP THE POSITIVE VALUE
+                   var addInt = multiple_type.Sum(x => Convert.ToInt32(item));
+                   Console.WriteLine(addInt);
+                   break;
+                }
+            }
+            
+            
+        }
+    }
+}
